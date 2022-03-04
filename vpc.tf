528604ed29c1affcdfe9f4eb34b07b378ab0c6e0
santosh <santu.kumar110@gmail.com>
2022-03-04 15:32:21 +0530

    Added IGW for VPC

--- vpc.tf
+++ vpc.tf
@@ -2,8 +2,15 @@ resource "aws_vpc" "default" {
     cidr_block = "172.19.0.0/16"
     enable_dns_hostnames = true
     tags = {
-    Name = "${var.vpc_name}"
-    Environment = "${var.environment}"
+    Name = "devopspract"
+    Environment = "Development"
     Owner = "santosh kumar"
     }
 }
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+	tags = {
+        Name = "devopspract-IGW"
+        
+    }
